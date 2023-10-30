4f003e865ee43c95c201489dcb53ee69bae25edb
Tobenna <tobirama@TobePC.localdomain>
2023-10-31 00:38:45 +0100

    task7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+# YOUR CODE GOES HERE. PLEASE REMOVE THis line
+# Comment test
+# LOL If this works
 print(f"First 3 letters: {word[:3]}")
 print(f"Last 2 letters: {word[7:]}")
 print(f"Middle word: {word[1:8]}")
