92ea8acd415884c00f9d962af598e20cec6d9365
Tobenna <tobirama@TobePC.localdomain>
2023-10-31 13:23:26 +0100

    task6.1

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,5 @@
 for i in range (0, 8):
     for j in range (i, 10):
         if (i == j): continue
-        print(i, end = "")
-        print(j, end = ", ")
+        print("{:d}{:d}".format(i, j), end = ", ")
 print("89\n")
