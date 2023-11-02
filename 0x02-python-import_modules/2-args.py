8950a4c2b07b5e82412b323f3e52a00744a5d702
Tobenna <tobirama@TobePC.localdomain>
2023-11-02 23:31:48 +0100

    task2.1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
-if (len(sys.argv) > 1):
-    print(f"{len(sys.argv) - 1} arguments:")
-else:
-     print(f"{len(sys.argv) - 1} arguments.")
-for j in range(len(sys.argv) - 1):
-    print(f"{j+1}: {sys.argv[j+1]}")
+if __name__ == "__main__":
+    if (len(sys.argv) > 1):
+        print(f"{len(sys.argv) - 1} arguments:")
+    else:
+        print(f"{len(sys.argv) - 1} arguments.")
+    for j in range(len(sys.argv) - 1):
+        print(f"{j+1}: {sys.argv[j+1]}")
 
