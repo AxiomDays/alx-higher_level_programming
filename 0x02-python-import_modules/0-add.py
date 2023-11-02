450c64d691bce0b09f3a733f8a74c82e15cb1cd2
Tobenna <tobirama@TobePC.localdomain>
2023-11-02 21:51:23 +0100

    task0.2

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0 as addmod
+import add_0
 
 a = 1
 b = 2
-print("{0} + {1} = {2}".format(a, b, addmod.add(a, b)))
+print("{0} + {1} = {2}".format(a, b, add_0.add(a, b)))
