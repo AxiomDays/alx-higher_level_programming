37ec2bba05631262ac203a00852462d7313c8022
Tobenna <tobirama@TobePC.localdomain>
2023-11-06 22:35:39 +0100

    task6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    c = 0
     for i in range(len(matrix)):
         for j in matrix[i]:
-            print("{:d}".format(j), end = " ")
+            if (c == (len(matrix[i]) - 1)):
+                print("{:d}".format(j), end = "")
+            else:
+                print("{:d}".format(j), end = " ")
+            c += 1
+        c = 0
         print("\n", end = "")
