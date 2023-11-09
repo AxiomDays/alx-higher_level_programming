eccbd4006216e371bc694f57379599ecd4c89a62
Tobenna <tobirama@TobePC.localdomain>
2023-11-09 02:40:50 +0100

    task?

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    nu = my_list.reduce(lambda a, b: a+b)
+    return nu
---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    dictlist = ((a_dictionary.values()))
+    print (dictlist)
+    
+
