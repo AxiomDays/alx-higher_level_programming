b92e7ad828b86b3333080985bf77d5c5318ee6c3
AxiomDays <tobeoke@gmail.com>
2023-11-22 03:34:47 +0100

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" creating a square with size priv attribute """
+
+
+class Square:
+    """
+        attributes: __size (int)
+    """
+
+    def __init__(self, size):
+        """
+            args: __size(int)
+        """
+        self.__size = size
+
