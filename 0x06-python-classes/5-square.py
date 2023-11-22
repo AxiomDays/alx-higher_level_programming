5f56ddb67cf5f57683fd2aca14feea55eaa19625
AxiomDays <tobeoke@gmail.com>
2023-11-22 04:01:37 +0100

    task5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"doc for square class with error check"
+
+class Square:
+    """
+        attributes:
+            size (int)
+    """
+    def __init__(self, size=0):
+        if type(size) is int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        area=(self.__size ** 2)
+        return area
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for j in range (self.__size):
+                    print("#", end="")
+                print("")
