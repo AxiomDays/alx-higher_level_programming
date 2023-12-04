7907ef9574daad859e065b2d488d28c476237def
AxiomDays <tobeoke@gmail.com>
2023-12-04 22:08:23 +0100

    task1.1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,9 +2,12 @@
 """ a class that inherits list """
 
 class MyList(list):
+    """ the class in question """
     def __init__(self):
+        """ useless init fxn """
         pass
     def print_sorted(self):
+        """ the printer fxn """
         print(sorted(self))
 
 
