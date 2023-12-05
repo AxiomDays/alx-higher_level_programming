611c09ff66b1f2d1da20cb6ed77ce6b09f4cb9a1
AxiomDays <tobeoke@gmail.com>
2023-12-05 11:17:46 +0100

    task4.1

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,3 +5,7 @@ def inherits_from(obj, a_class):
     if issubclass(type(obj), a_class):
         if (type(obj) != a_class):
             return True
+        else:
+            return False
+    else:
+        return False
