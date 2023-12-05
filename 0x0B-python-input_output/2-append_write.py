f2f7f4f6d5da667b1d91bdcf2920e6d738e535c6
AxiomDays <tobeoke@gmail.com>
2023-12-05 20:19:48 +0100

    task

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+""" function that appends """
 def append_write(filename="", text=""):
+    """ the function in question """
     with open(filename, 'a', encoding="utf-8") as h:
         return h.write(text)
