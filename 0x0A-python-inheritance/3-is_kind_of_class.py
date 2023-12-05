6d59736bbe39860148294a662d19bd457030c206
AxiomDays <tobeoke@gmail.com>
2023-12-05 11:00:01 +0100

    task3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+""" function to check if instance is same as class """
+def is_kind_of_class(obj, a_class):
+    """ the function in question """
+    return(isinstance(obj, a_class))
