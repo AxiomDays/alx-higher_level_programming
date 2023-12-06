5a0d5744fd1ed0376574f0ab59289d98c722e73c
AxiomDays <tobeoke@gmail.com>
2023-12-06 01:17:53 +0100

    task8.2

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,14 +1,8 @@
-#!/usr/bin/bash
+#!/usr/bin/python3
 """ this is what this file does """
 
 def class_to_json(obj):
     """
     Convert an object into a dictionary with simple data structures for JSON serialization.
-
-    Args:
-    - obj: The object to be converted.
-
-    Returns:
-    - A dictionary with simple data structures.
     """
     return obj.__dict__
