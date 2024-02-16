21c531dbbd4658b6aec8e527d65049ecc2070aa8
AxiomDays <tobeoke@gmail.com>
2024-02-16 22:13:08 +0100

    task6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+/* a script that prints 3 lines using an array */
+
+let printarr = ["C is fun", "Python is cool", "Javascript is amazing"];
+for (let i = 0; i < 3; i++){
+	console.log(printarr[i]);
+}
