cc2fb86cd1ee9d4cc777329a0ec3f8077dc72f1e
AxiomDays <tobeoke@gmail.com>
2024-02-16 22:22:11 +0100

    task8

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/* a script that prints x times that c is fun */
+
+if (!isNaN(parseInt(process.argv[2]))){
+	for (let i = 0; i < process.argv[2]; i++){
+		console.log("C is fun")
+	}
+} else {
+	console.log("Missing number of occurrences")
+}
