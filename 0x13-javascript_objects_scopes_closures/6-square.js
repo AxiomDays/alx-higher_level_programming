1f0c32a6163e9b0d1086663afc261a4bf2e3160b
AxiomDays <tobeoke@gmail.com>
2024-02-17 12:20:34 +0100

    task6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,27 +3,27 @@
 const rect = require('./5-square');
 
 class Square extends rect {
-        constructor (size){
-                super(size, size);
-        }
+  constructor (size) {
+    super(size, size);
+  }
 
-	charPrint (c){
-		if (typeof(c) !== 'undefined'){
-			 for (let i = 0; i < this.height; i++) {
-				 for (let j = 0; j < this.width; j++) {
-					 process.stdout.write(c);
-				 }
-				 console.log('');
-			 }	
-		} else {
-			for (let i = 0; i < this.height; i++) {
-                                 for (let j = 0; j < this.width; j++) {
-                                         process.stdout.write('X');
-                                 }
-                                 console.log('');
-                         }
-		}
-	}
+  charPrint (c) {
+    if (typeof (c) !== 'undefined') {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write(c);
+        }
+        console.log('');
+      }
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write('X');
+        }
+        console.log('');
+      }
+    }
+  }
 }
 
 module.exports = Square;
