31d9a3aa2f3fd2d7130869cd52fb86a37619780d
AxiomDays <tobeoke@gmail.com>
2024-03-16 13:07:50 +0100

    task101

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #post parameters
-curl --data "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" $1
+curl -s --data "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" $1
