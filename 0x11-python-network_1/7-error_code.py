966c211f483defb104144411309ce162bb747f21
AxiomDays <tobeoke@gmail.com>
2024-03-16 15:41:31 +0100

    task7.2

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     try:
         r = requests.get(sys.argv[1])
         print(r.text)
-    except r.status_code as e:
-        print("Error code: {}".format(e))
+    except requests.exceptions.RequestException as e:
+        print("Error code: {}".format(r.status_code))
