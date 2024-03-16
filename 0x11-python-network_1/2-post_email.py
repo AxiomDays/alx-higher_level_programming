bf80a1a386a80ecced741686d4fb2b53a693d05e
AxiomDays <tobeoke@gmail.com>
2024-03-16 14:01:15 +0100

    task2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""takes in a url and sends email"""
+if __name__ == "__main__":
+    import urllib.parse
+    import urllib.request
+    import sys
+
+    
+    values = {'email' : sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    r = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(r) as resp:
+        page = resp.read()
+    print(page.encode('utf-8'))
